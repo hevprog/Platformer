cbc85dce0ada9ae33b7fd7d80508d2efdfba3cee
Nico Timothy <142752571+NicoTimothy11@users.noreply.github.com>
2024-10-04 21:40:34 +0800

    Recent Changes

--- main.cpp
+++ main.cpp
@@ -4,15 +4,13 @@
 
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
+    
     const int screenWidth = 800;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "Platformers");
 
-    // NOTE: Be careful, background width must be equal or bigger than screen width
-    // if not, texture should be draw more than two times for scrolling effect
+    
     Texture2D background = LoadTexture("Graphics/BG/back.png");
     Texture2D midground = LoadTexture("Graphics/BG/middle.png");
     Texture2D foreground = LoadTexture("Graphics/BG/foreground.png");
@@ -21,56 +19,44 @@ int main(void)
     float scrollingMid = 0.0f;
     float scrollingFore = 0.0f;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    SetTargetFPS(60);               
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
+        
         scrollingBack -= 0.1f;
         scrollingMid -= 0.5f;
         scrollingFore -= 1.0f;
 
-        // NOTE: Texture is scaled twice its size, so it sould be considered on scrolling
         if (scrollingBack <= -background.width*2) scrollingBack = 0;
         if (scrollingMid <= -midground.width*2) scrollingMid = 0;
         if (scrollingFore <= -foreground.width*2) scrollingFore = 0;
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
+        
         BeginDrawing();
 
             ClearBackground(GetColor(0x052c46ff));
 
-            // Draw background image twice
-            // NOTE: Texture is scaled twice its size
+    
             DrawTextureEx(background, (Vector2){ scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
             DrawTextureEx(background, (Vector2){ background.width*2 + scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
 
-            // Draw midground image twice
+            
             DrawTextureEx(midground, (Vector2){ scrollingMid, 20 }, 0.0f, 2.0f, WHITE);
             DrawTextureEx(midground, (Vector2){ midground.width*2 + scrollingMid, 20 }, 0.0f, 2.0f, WHITE);
 
-            // Draw foreground image twice
             DrawTextureEx(foreground, (Vector2){ scrollingFore, 70 }, 0.0f, 2.0f, WHITE);
             DrawTextureEx(foreground, (Vector2){ foreground.width*2 + scrollingFore, 70 }, 0.0f, 2.0f, WHITE);
 
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
+        
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadTexture(background);  // Unload background texture
-    UnloadTexture(midground);   // Unload midground texture
-    UnloadTexture(foreground);  // Unload foreground texture
+    UnloadTexture(background);
+    UnloadTexture(midground);  
+    UnloadTexture(foreground); 
 
-    CloseWindow();              // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow();        
 
     return 0;
 }
